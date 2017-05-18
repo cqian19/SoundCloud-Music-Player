f6230530136344e23a23bb27c98b66c3f5c98f9c
Christopher <cqian019@gmail.com>
2017-05-18 00:18:42 -0700

    Added files

---
+++ app/components/footer.component.js
@@ -0,0 +1,17 @@
+/**
+ * Created by cqian19 on 5/17/2017.
+ */
+import React from 'react';
+
+class Footer extends React.Component {
+
+    render(){
+        return (
+            <div className="footer">
+                <p>Love from <img src="public/img/soundcloud.png" className="soundcloud"/></p>
+            </div>
+        )
+    }
+}
+
+export default Footer
